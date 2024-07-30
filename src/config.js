bc471607508e7ad281964357a3f1acebd436f881
hashim_mac_m3_air <taylorsuccessor@gmail.com>
2024-07-30 18:02:23 +0300

    TASK-07: fix API url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api/',
+  apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'https://tech-test.livaatverse.com/api/',
   imageBaseUrl: process.env.REACT_APP_IMG_BASE_URL || 'https://file-tech-test.livaatverse.com/',
 };
 
